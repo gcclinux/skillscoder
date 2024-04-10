9194e0274f6cf21d2b0a9879fc927d84f4878f0c
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-04-10 11:55:21 +0100

    Wed, Apr 10, 2024 11:55:21 AM

--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 )
 
 func main() {
+
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
 	r.LoadHTMLFiles("html/index.html") // load the HTML file
@@ -23,6 +25,27 @@ func main() {
 			"Title": "Welcome to OnlineDev!",
 			"Body":  template.HTML(content), // convert the content to HTML
 		}) // render the HTML file with data
+	})
+
+	r.Static("/js", "js")     // serve the js folder
+	r.Static("/code", "code") // serve the js folder
+
+	r.POST("/code/test.go", func(c *gin.Context) {
+		var json struct {
+			Content string `json:"content"`
+		}
+
+		if err := c.BindJSON(&json); err == nil {
+			path := "./code/test.go"
+			err := os.WriteFile(path, []byte(json.Content), 0644)
+			if err != nil {
+				c.JSON(500, gin.H{"status": "unable to save file"})
+				return
+			}
+			c.JSON(200, gin.H{"status": "file saved"})
+		} else {
+			c.JSON(400, gin.H{"status": "unable to bind JSON"})
+		}
 	})
 
 	r.Run("localhost:2000") // listen and serve on localhost:2000
