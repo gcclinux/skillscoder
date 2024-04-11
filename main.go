5466055ce5bc7d3559f1129f0a01cb859b30e295
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-04-11 11:44:05 +0100

    Thu, Apr 11, 2024 11:44:05 AM

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"fmt"
-	"html/template"
-	"log"
-	"os"
-	"os/exec"
+	"ricardow/routes"
 
 	"github.com/gin-gonic/gin"
 )
@@ -17,51 +14,9 @@ func main() {
 
 	r.LoadHTMLFiles("html/index.html") // load the HTML file
 
-	r.GET("/", func(c *gin.Context) {
-		content, err := os.ReadFile("html/body.html") // read the body content from a file
-		if err != nil {
-			log.Fatal(err)
-		}
+	routes.SetupRoutes(r)
 
-		c.HTML(200, "index.html", gin.H{
-			"Title": "Welcome to Skills Club Coder!",
-			"Body":  template.HTML(content), // convert the content to HTML
-		}) // render the HTML file with data
-	})
-
-	r.Static("/js", "js")     // serve the js folder
-	r.Static("/code", "code") // serve the js folder
-	r.Static("/css", "css")   // serve the js folder
-
-	r.POST("/code/test.go", func(c *gin.Context) {
-		var json struct {
-			Content string `json:"content"`
-		}
-
-		if err := c.BindJSON(&json); err == nil {
-			path := "./code/test.go"
-			err := os.WriteFile(path, []byte(json.Content), 0644)
-			if err != nil {
-				log.Printf("Error writing file: %v", err)
-				c.JSON(500, gin.H{"status": "unable to save file"})
-				return
-			}
-			// Run the command after the file is saved
-			cmd := exec.Command("go", "run", path)
-			output, err := cmd.CombinedOutput()
-			if err != nil {
-				log.Printf("Error running command: %v", err)
-				c.JSON(500, gin.H{"status": "unable to run command", "error": err.Error()})
-				return
-			}
-
-			outputStr := string(output) // convert output to string
-			c.JSON(200, gin.H{"status": "file saved and command run", "output": outputStr})
-		} else {
-			c.JSON(400, gin.H{"status": "unable to bind JSON"})
-		}
-	})
 	address := "0.0.0.0:8000"
-	fmt.Printf("\nServer is running at http://%s\n\n", address)
+	fmt.Printf("\nServer is running at http://%s\n\n", "localhost:8000")
 	r.Run(address)
 }
---
+++ routes/routes.go
@@ -0,0 +1,58 @@
+package routes
+
+import (
+	"html/template"
+	"log"
+	"os"
+	"os/exec"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRoutes(r *gin.Engine) {
+	r.GET("/", func(c *gin.Context) {
+		content, err := os.ReadFile("html/body.html") // read the body content from a file
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		c.HTML(200, "index.html", gin.H{
+			"Title": "Welcome to Skills Club Coder!",
+			"Body":  template.HTML(content), // convert the content to HTML
+		}) // render the HTML file with data
+	})
+
+	r.Static("/js", "js")     // serve the js folder
+	r.Static("/code", "code") // serve the code folder
+	r.Static("/css", "css")   // serve the css folder
+	r.Static("/img", "img")   // serve the images folder
+
+	r.POST("/code/test.go", func(c *gin.Context) {
+		var json struct {
+			Content string `json:"content"`
+		}
+
+		if err := c.BindJSON(&json); err == nil {
+			path := "./code/test.go"
+			err := os.WriteFile(path, []byte(json.Content), 0644)
+			if err != nil {
+				log.Printf("Error writing file: %v", err)
+				c.JSON(500, gin.H{"status": "unable to save file"})
+				return
+			}
+			// Run the command after the file is saved
+			cmd := exec.Command("go", "run", path)
+			output, err := cmd.CombinedOutput()
+			if err != nil {
+				log.Printf("Error running command: %v", err)
+				c.JSON(500, gin.H{"status": "unable to run command", "error": err.Error()})
+				return
+			}
+
+			outputStr := string(output) // convert output to string
+			c.JSON(200, gin.H{"status": "file saved and command run", "output": outputStr})
+		} else {
+			c.JSON(400, gin.H{"status": "unable to bind JSON"})
+		}
+	})
+}
