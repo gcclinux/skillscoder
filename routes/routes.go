beddb412ce920cb56b00ecfe26c6e2dbb83cc9f4
Ricardo Wagemaker <java@wagemaker.co.uk>
2024-05-22 15:48:31 +0100

    ongoing preparation for multiuser

--- routes/routes.go
+++ routes/routes.go
@@ -27,7 +27,18 @@ func SetupRoutes(r *gin.Engine) {
 		c.HTML(200, "index.html", gin.H{
 			"Title":     "SkillsCoder",
 			"SubTitle":  "Welcome to SkillsCoder! (Concept - build 006)",
-			"GlobalVar": globalVar, // use the global variable
+			"GlobalVar": globalVar,
+			"RetrieveCode": `package main
+import "fmt"
+
+func main() {
+	var num1 int = 10
+	var num2 int = 20
+	var num3 int = 30
+	var total int = (num1 + num2 - num3)
+
+	fmt.Println("Total: ",total)
+}`,
 		}) // render the HTML file with data
 	})
 
